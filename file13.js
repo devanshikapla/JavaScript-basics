5d89eb68236c5aed6ac5e77efef66a472755cb2e
Devanshi Kapla <89089904+devanshikapla@users.noreply.github.com>
2022-07-05 21:12:35 +0530

    Add files via upload

---
+++ file13.js
@@ -0,0 +1,15 @@
+//truthy and falsy values
+
+//falsyvalues
+//false
+//""
+//null 
+//undefined
+//0
+let firstname="";
+if(firstname){
+    console.log(firstname);
+}
+else{
+    console.log("empty value");
+}
---
+++ file14.js
@@ -0,0 +1,22 @@
+
+//ifelse
+let age=16;
+
+if(age > 18){
+    console.log("user can play game");
+
+}
+else
+{
+    console.log("user cannot play game");
+
+}
+
+let num=6;
+//console.log(6%2);
+if(num%2==0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
